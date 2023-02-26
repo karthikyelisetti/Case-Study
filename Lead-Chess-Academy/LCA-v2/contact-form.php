787dd63a3f4c15d950ff2b7d8c8768363f2d28cc
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-02-27 01:25:14 +0530

    updated the files

--- Lead-Chess-Academy/LCA-v2/contact-form.php
+++ Lead-Chess-Academy/LCA-v2/contact-form.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 //////////////////////////
 //Specify default values//
 //////////////////////////
@@ -27,47 +27,47 @@ $errors = array();
 //"name" field required by this PHP script even if 
 // there are no 'aria-required="true"' or 'required' 
 // attributes on this HTML input field
-if(isset($_POST['name'])) {
-	
-	if(!empty($_POST['name']))
-		$sender_name  = stripslashes(strip_tags(trim($_POST['name'])));
-	
-	if(!empty($_POST['message']))
-		$message      = stripslashes(strip_tags(trim($_POST['message'])));
-	
-	if(!empty($_POST['email']))
+if (isset($_POST['name'])) {
+
+	if (!empty($_POST['name']))
+		$sender_name = stripslashes(strip_tags(trim($_POST['name'])));
+
+	if (!empty($_POST['message']))
+		$message = stripslashes(strip_tags(trim($_POST['message'])));
+
+	if (!empty($_POST['email']))
 		$sender_email = stripslashes(strip_tags(trim($_POST['email'])));
-	
-	if(!empty($_POST['subject']))
-		$subject      = stripslashes(strip_tags(trim($_POST['subject'])));
+
+	if (!empty($_POST['subject']))
+		$subject = stripslashes(strip_tags(trim($_POST['subject'])));
 
 
 	//Message if no sender name was specified
-	if(empty($sender_name)) {
+	if (empty($sender_name)) {
 		$errors[] = $name_not_specified;
 	}
 
-	$from = "MIME-Version: 1.0" . "\r\n" ;
+	$from = "MIME-Version: 1.0" . "\r\n";
 	$from .= "Content-Type: text/html; charset=UTF-8" . "\r\n";
-	$from .= (!empty($sender_email)) ? 'From: '.$sender_email : '';
+	$from .= (!empty($sender_email)) ? 'From: ' . $sender_email : '';
 
 	$subject = (!empty($subject)) ? $subject : $default_subject;
 
 
 	//sending message if no errors
-	if(empty($errors)) {
-		
+	if (empty($errors)) {
+
 		//duplicating email meta (from and subject) to email message body
 		$message_meta = '';
-			//From name and email
-		$message_meta .= 'From: '. $sender_name . ' ' . $sender_email . "<br>";
-			//Subject or default subject
-		$message_meta .= 'Subject: '. ( $subject ? $subject : $default_subject ) . "<br>";
+		//From name and email
+		$message_meta .= 'From: ' . $sender_name . ' ' . $sender_email . "<br>";
+		//Subject or default subject
+		$message_meta .= 'Subject: ' . ($subject ? $subject : $default_subject) . "<br>";
 
 		//adding another CUSTOM contact form fields that added by user to email message body
 		foreach ($_POST as $key => $value) {
 			//checking for standard fields 
-			if ($key == 'name' || $key == 'message' || $key == 'subject' || $key == 'email'  ) {
+			if ($key == 'name' || $key == 'message' || $key == 'subject' || $key == 'email') {
 				continue;
 			}
 			//adding key-value pare to email message body
@@ -76,15 +76,15 @@ if(isset($_POST['name'])) {
 
 		$message = $message_meta . "<br>" . 'Message:' . "<br>" . $message;
 		$message = wordwrap($message, 70);
-	
+
 		if (mail($your_email, $subject, $message, $from)) {
 			echo $email_was_sent;
 		} else {
 			$errors[] = $server_not_configured;
-			echo '<span class="form-errors">' . implode('<br>', $errors ) . '</span>';
+			echo '<span class="form-errors">' . implode('<br>', $errors) . '</span>';
 		}
 	} else {
-		echo '<span class="form-errors">' . implode('<br>', $errors ) . '</span>';
+		echo '<span class="form-errors">' . implode('<br>', $errors) . '</span>';
 	}
 } else {
 	// if "name" var not send ('name' attribute of contact form input field was changed or missing)
